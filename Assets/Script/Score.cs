83ae97618c48791c95605804e8ecfbd348993821
carloscrdevelop <diseno3dyarte@gmail.com>
2026-05-19 02:04:06 +0200

    DiceGame

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    DiceRoll dice;
+    [SerializeField] Text scoreText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        dice = FindObjectOfType<DiceRoll>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dice != null)
+        {
+            if (dice.diceFaceNum !=0)
+            {
+                scoreText.text = dice.diceFaceNum.ToString();
+            }
+        }   
+    }
+}
