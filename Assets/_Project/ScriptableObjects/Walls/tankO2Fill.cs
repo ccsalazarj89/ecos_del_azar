61852189bab36a774e1dede725e2f15d97fececb
diseno3dyarte-source <diseno3dyarte@gmail.com>
2026-08-13 18:59:08 +0200

    update

--- Assets/_Project/ScriptableObjects/Walls/tankO2Fill.cs
+++ Assets/_Project/ScriptableObjects/Walls/tankO2Fill.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
-
+using EcosDelAzar.Core;
 public class tankO2Fill : MonoBehaviour
 {
     private Renderer rend;
-    public int alpha;
+   // public int alpha;
+    OxygenTank oxygenTank;
+     
+    void Start()
+        {
+            var gm = GameManager.Instance;
+            if (gm == null) return;
 
+             oxygenTank = gm.OxygenTank;
+
+            if (oxygenTank != null)
+            {
+                oxygenTank.OnOxygenChanged += UpdateOxygen;
+                UpdateOxygen(oxygenTank.Current);
+            }
+        }
     void Awake()
     {
         rend = GetComponent<Renderer>();
     }
 
-    public void Update()
+    public void UpdateOxygen(float o2)
     {
         Color colorActual = rend.material.color;
-        colorActual.a = alpha; // valor entre 0 (invisible) y 1 (visible)
+        colorActual.a = o2; // valor entre 0 (invisible) y 1 (visible)
         rend.material.color = colorActual;
     }
 }
